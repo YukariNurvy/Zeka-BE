8474d73289caaab1f091cb6282237bedc682698e
YukariNurvy <ryanmarbun19@gmail.com>
2026-08-20 04:26:33 +0700

    Fix CORS error

--- zeka-game-lab-backend/backend/server.js
+++ zeka-game-lab-backend/backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
+const cors = require('cors');
 const express = require("express");
-const cors = require("cors");
+app.use(cors());
 const path = require("path");
 
 const authRoutes = require("./routes/auth");
